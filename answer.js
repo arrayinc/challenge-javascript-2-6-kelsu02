26c8fea25a65ce485d0d3f6295986d628084a32d
kredmond <kelsu02@gmail.com>
2019-11-21 14:52:53 -0700

    How tall are you in go dog go books.

--- answer.js
+++ answer.js
@@ -1,1 +1,6 @@
 // *** YOUR ANSWER BELOW ***
+function goDogGoHeight(feet,inches) {
+    return "You are " + Math.floor((feet * 12 + inches )/ 0.6) + " Go, Dog. Go! books tall";
+}
+
+console.log(goDogGoHeight(5,10));
